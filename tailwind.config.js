d4bb38e3e42b1da90d53b63728a56610a5e92924
Caio Queiroz Soares <caioqs.info@gmail.com>
2025-04-10 11:00:14 -0300

    Retoques finais

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./*.html"],
   theme: {
     extend: {
       colors: {
         primary: '#005CFF',
+        blueSecondary: '#00264E',
         purplePrimary: '#6220C1',
         grayPrimary: '#E7E7EA',
         graySecondary: '#DDDDDD',
